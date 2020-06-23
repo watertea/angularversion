85a4fa3f84a58982c9a96d1c48bcb10cd60b0133
weiss <1297663809@qq.com>
2020-06-23 14:22:21 +0800

    pref: major

--- projects/my-lib/src/lib/my-lib.component.ts
+++ projects/my-lib/src/lib/my-lib.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'lib-my-lib',
   template: `
     <p>
-      my-lib works22225465756!
+      my-lib works2222546575678786786!
     </p>
   `,
   styles: [
