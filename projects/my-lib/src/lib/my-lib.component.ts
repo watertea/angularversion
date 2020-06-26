e6141b1aeba30cb61ae747a74f0e5c0b8137f3d1
weiss <1297663809@qq.com>
2020-06-26 16:38:10 +0800

    new test

--- projects/my-lib/src/lib/my-lib.component.ts
+++ projects/my-lib/src/lib/my-lib.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'lib-my-lib',
   template: `
     <p>
-      my-lib works222254657567!
+      my-lib works222254657dfdf567!
     </p>
   `,
   styles: [
